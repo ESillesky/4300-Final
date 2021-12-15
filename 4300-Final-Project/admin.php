6dfae4930470d76af98af20431ec939e2a56c3d9
cdj26643 <cdj26643@uga.edu>
2021-12-14 19:18:39 -0500

    Update admin.php

--- 4300-Final-Project/admin.php
+++ 4300-Final-Project/admin.php
@@ -5,14 +5,12 @@ $username='eliza';
 $password='password';
 
 try {
-    echo 'Attempting Connection';
     $db=new PDO($dsn, $username,$password);
-    echo 'Connected successfully';
     }
     catch (PDOException  $e)
     {
         $error=$e->getMessage();
-        echo  '<p> Unable to connec to the database: '.$error;
+        echo  '<p> Unable to connect to the database: '.$error;
         exit();
      }
 
@@ -22,12 +20,122 @@ echo $_SESSION["username"];
 
 <!DOCTYPE html>
 <html>
+<head>
 
+</head>
 <body>
-    <p>Admin page</p>
-    <p><a href ="logout.php">Logout</a></p>
-    <p><a href ="addTicket.php">Add Event</a></p>
-    <p><a href ="deleteTicket.php">Delete Event</a></p>
 
+<div class="admin-section">
+  <h1>Admin Page</h1>
+</div>
+
+<h2 style="text-align:center">Welcome, Admin!</h2>
+<div class="row" style="text-align:center">
+  <div class="column">
+    <div class="card">
+      <img src="localImages/ad/logout.png" width="250" height="250">
+      <div class="container">
+        <h2><a href ="logout.php">Logout</a></h2>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="localImages/ad/ticket+.png" width="250" height="250">
+      <div class="container">
+        <h2><a href ="addTicket.php">Add Event</a></h2>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="localImages/ad/ticket-.png" width="250" height="250">
+      <div class="container">
+        <h2><a href ="deleteTicket.php">Delete Event</a></h2>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
 </html>
+
+<head>
+<style>
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+html {
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.column {
+  float: left;
+  width: 33.3%;
+  margin-bottom: 16px;
+  padding: 0 8px;
+}
+
+.ticket {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  margin: 8px;
+}
+
+.admin-section {
+  padding: 50px;
+  text-align: center;
+  background-color: #474e5d;
+  color: white;
+}
+
+/* unvisited link */
+a:link {
+  color: red;
+}
+
+/* visited link */
+a:visited {
+  color: red;
+}
+
+/* mouse over link */
+a:hover {
+  color: black;
+}
+
+/* selected link */
+a:active {
+  color: blue;
+}
+
+.container {
+  padding: 0 16px;
+}
+
+.container::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color: grey;
+}
+
+@media screen and (max-width: 650px) {
+  .column {
+    width: 100%;
+    display: block;
+  }
+}
+
+</style>
+</head>
